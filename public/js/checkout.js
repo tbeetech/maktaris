ec107108a4cbd03cbf9993f35e2808d2f8c8de75
Oluwatobi <tobirammar@gmail.com>
2021-06-04 07:58:58 +0100

    edition finale

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -78,7 +78,8 @@ let payStack;
 
 
 
-
+dbt = document.querySelector("#customRadio1").checked;
+console.log("xxxxxxxxxxx", dbt)
 
 let payButton = document.querySelector(".payaction");
 payButton.addEventListener("click", (e)=> {
@@ -90,7 +91,7 @@ payButton.addEventListener("click", (e)=> {
     // localStorage.setItem('phone', phone)
     lastName = document.querySelector("#lname").value;
     firstName = document.querySelector("#fname").value;
-    dbt = document.querySelector("#customRadio1").value;
+    dbt = document.querySelector("#customRadio1").checked;
     address = document.querySelector("#address").value;
     address2 = document.querySelector("#address2").value;
     zip = document.querySelector("#zippostalcode").value;
@@ -99,63 +100,67 @@ payButton.addEventListener("click", (e)=> {
 
     console.log("radio hahaha: " + dbt);
     
+    if(dbt) {
+        subject = "Testing Maktaris Herbals";
+        // text = dbt + " " + phone + " " + " " + lastName + " " + firstName;
     
-    subject = "Testing Maktaris Herbals";
-    // text = dbt + " " + phone + " " + " " + lastName + " " + firstName;
-
-    //.. Ready html string to parse as text to sendmail options.
-    var text = `<div style="height: 100vh; border: 1px solid rgb(41, 216, 41); border-radius: 2px; font-family:Verdana, Geneva, Tahoma, sans-serif; color: grey; font-size: small;">      
-    <div style="margin: 8px;">
-     <div style="width:auto; height:50px; text-align: center; color: white; font-size: 20px; background-color:greenyellow;"> <p style="margin:0; position: relative; top: 10px;">Thank you for your Order!</p> </div>
-     <p>Hi ${firstName}</p>
-     <p style="margin-bottom: 1;">Thanks for your order. It’s on-hold until we confirm that payment has been received. In the meantime, here’s a reminder of what you ordered:</p>
-     <strong style="font-style: oblique; ">SEND PROOF OF PAYMENT TO THIS WHATSAPP NUMBER:</strong>
-     <p style="margin: 0;">+2347039773982</p>
-     <h5 style="color: orange;">Our Bank details:</h5>
-     <strong style="color: green;">MAKTARIS HERBALS</strong>
-     <ul>
-         <li>Bank: <strong style="font-size: x-small;">First Bank plc</strong></li>
-         <li>Account number: <strong style="font-size: x-small;">2034776995</strong></li>
-         <li>Account Name: <strong style="font-size: x-small;">Maktaris signature</strong></li>
-         <!--Fetch order date here...-->
-         <h3 style="text-decoration: underline;">${today}</h3>
-         <h2 style="color: green;text-decoration: underline;">Your Order:</h2>
-         <!--Table for product draft should be created from checkout page as an indpendent string... and templated into main html string-->                    
-     </ul>
-
-     ${infodiv}
-<div>
- <div style="width: 160px; display:inline-flex">
-     <h6 style="border: 1px solid green; color: red;">${address} ${zip}, ${city}, ${state}<p>${phone}, ${email}</p></h6>
-   
-     
-     
-     
-
-     </div>
-     <div style="width: 160px; display: inline-flex;">
-     <h6 style="border: 1px solid green; color: red;">${address2} ${zip}, ${city}, ${state}<p>${phone}, ${email}</p></h6>
-         </div>          
-</div>
+        //.. Ready html string to parse as text to sendmail options.
+        var text = `<div style="height: 100vh; border: 1px solid rgb(41, 216, 41); border-radius: 2px; font-family:Verdana, Geneva, Tahoma, sans-serif; color: grey; font-size: small;">      
+        <div style="margin: 8px;">
+         <div style="width:auto; height:50px; text-align: center; color: white; font-size: 20px; background-color:greenyellow;"> <p style="margin:0; position: relative; top: 10px;">Thank you for your Order!</p> </div>
+         <p>Hi ${firstName}</p>
+         <p style="margin-bottom: 1;">Thanks for your order. It’s on-hold until we confirm that payment has been received. In the meantime, here’s a reminder of what you ordered:</p>
+         <strong style="font-style: oblique; ">SEND PROOF OF PAYMENT TO THIS WHATSAPP NUMBER:</strong>
+         <p style="margin: 0;">+2347039773982</p>
+         <h5 style="color: orange;">Our Bank details:</h5>
+         <strong style="color: green;">MAKTARIS HERBALS</strong>
+         <ul>
+             <li>Bank: <strong style="font-size: x-small;">First Bank plc</strong></li>
+             <li>Account number: <strong style="font-size: x-small;">2034776995</strong></li>
+             <li>Account Name: <strong style="font-size: x-small;">Maktaris signature</strong></li>
+             <!--Fetch order date here...-->
+             <h3 style="text-decoration: underline;">${today}</h3>
+             <h2 style="color: green;text-decoration: underline;">Your Order:</h2>
+             <!--Table for product draft should be created from checkout page as an indpendent string... and templated into main html string-->                    
+         </ul>
+    
+         ${infodiv}
+    <div>
+     <div style="width: 160px; display:inline-flex">
+         <h6 style="border: 1px solid green; color: red;">${address} ${zip}, ${city}, ${state}<p>${phone}, ${email}</p></h6>
+       
+         
+         
+         
+    
+         </div>
+         <div style="width: 160px; display: inline-flex;">
+         <h6 style="border: 1px solid green; color: red;">${address2} ${zip}, ${city}, ${state}<p>${phone}, ${email}</p></h6>
+             </div>          
     </div>
-     <!-- <div style="width: 180px;">
-         <h1 style="border: 1px solid green; color: red; position: relative; left: 200px;">Total amount</h1>
-     </div> -->
-
-
- </div>`
-
+        </div>
+         <!-- <div style="width: 180px;">
+             <h1 style="border: 1px solid green; color: red; position: relative; left: 200px;">Total amount</h1>
+         </div> -->
     
-    console.log(email, phone, lastName, firstName)
-    const data = {
-        email,
-        subject,
-        text
-      };
-
-      $.post("/product-checkout", data, function() {
-        console.log("Server recieved our data")
-      })
+    
+     </div>`
+    
+        
+        console.log(email, phone, lastName, firstName)
+        const data = {
+            email,
+            subject,
+            text
+          };
+    
+          $.post("/product-checkout", data, function() {
+            console.log("Server recieved our data")
+          })
+    } else {
+        alert("Please tick direct bank transfer to proceed...")
+    }
+  
     //   alert("You message as been recieved. You will receive response shortly")
 })
 
