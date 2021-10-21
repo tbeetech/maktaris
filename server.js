c13c6f01f19798980bef044855149e2712fd7d13
tbee <tobirammar@gmail.com>
2021-10-21 11:27:10 -0700

    m

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.set('view engine', 'ejs')
 app.get('*', function(req, res) {  
     res.redirect('https://' + req.headers.host + req.url);
 });
-app.enable('trust proxy')
-app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
-})
+// app.enable('trust proxy')
+// app.use((req, res, next) => {
+//     req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+// })
 app.post('/product-checkout', (req, res)=> {
     // res.json({message: "Message recieved"})
     // console.log("something got posted")-
