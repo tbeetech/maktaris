ec390d1f67057b7549b9e46ea54dbf0af659da06
TbeeTech <tobirammar@gmail.com>
2022-04-29 16:19:05 +0100

    super works

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/contact', (req, res)=> {
 })
 
 app.get('/', (req,res)=> {
-    res.render('pages/index', {finalDate:finalDate})
+    res.render('index', {finalDate:finalDate})
 })
 app.get('/about', (req, res)=>{
     res.render('about', {finalDate:finalDate})
